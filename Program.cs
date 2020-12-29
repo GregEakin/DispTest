a7ce894aa9bc9042863a5a7e8cf76adc5d6f284c
Greg Eakin <gregory.eakin@gmail.com>
2020-12-28 16:51:36 -0800

    Add cusotom codes.

--- Program.cs
+++ Program.cs
@@ -9,16 +9,44 @@ namespace tmp102
 
     class Program
     {
+        public static void ShowSpecialSymbols(Hd44780 lcd)
+        {
+            // We have space for eight custom characters
+            var bell = new byte[] { 0x04, 0x0e, 0x0e, 0x0e, 0x1f, 0x00, 0x04 };
+            var note = new byte[] { 0x02, 0x03, 0x02, 0x0e, 0x1e, 0x0c, 0x00 };
+            var clock = new byte[] { 0x00, 0x0e, 0x15, 0x17, 0x11, 0x0e, 0x00 };
+            var heart = new byte[] { 0x00, 0x0a, 0x1f, 0x1f, 0x0e, 0x04, 0x00 };
+            var duck = new byte[] { 0x00, 0x0c, 0x1d, 0x0f, 0x0f, 0x06, 0x00 };
+            var check = new byte[] { 0x00, 0x01, 0x03, 0x16, 0x1c, 0x08, 0x00 };
+            var cross = new byte[] { 0x00, 0x1b, 0x0e, 0x04, 0x0e, 0x1b, 0x00 };
+            var retArrow = new byte[] { 0x01, 0x01, 0x05, 0x09, 0x1f, 0x08, 0x04 };
+
+            lcd.CreateCustomCharacter(0, bell);
+            lcd.CreateCustomCharacter(1, note);
+            lcd.CreateCustomCharacter(2, clock);
+            lcd.CreateCustomCharacter(3, heart);
+            lcd.CreateCustomCharacter(4, duck);
+            lcd.CreateCustomCharacter(5, check);
+            lcd.CreateCustomCharacter(6, cross);
+            lcd.CreateCustomCharacter(7, retArrow);
+
+            lcd.SetCursorPosition(0, 3);
+            var msg = $"\x0\x1\x2\x3\x4\x5\x6\x7 \x8\x9\xA";
+            lcd.Write(msg);
+        }
+
         public static void Main(string[] args)
         {
             var settings = new I2cConnectionSettings(0x00, 0x27);
             using var device = new UnixI2cDevice(settings);
             using var lcd = new Hd44780(new Size(20, 4), new I2C4Bit(device));
-            
+
             var name0 = File.ReadAllText("/sys/class/thermal/thermal_zone0/type").Substring(0, 3);
             var name1 = File.ReadAllText("/sys/class/thermal/thermal_zone1/type").Substring(0, 3);
 
             lcd.BacklightOn = true;
+            ShowSpecialSymbols(lcd);
+
             for (var i = 0; i < 200; i++)
             {
                 var temp0 = File.ReadAllText("/sys/class/thermal/thermal_zone0/temp");
