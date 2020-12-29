5b0a126bfdb47a2292a2fa9da073998a7f380f0f
Greg Eakin <gregory.eakin@gmail.com>
2020-12-28 16:10:05 -0800

    Refactor backlight flag.

--- I2c4Bit.cs
+++ I2c4Bit.cs
@@ -30,7 +30,6 @@ namespace tmp102
         public I2C4Bit(I2cDevice device)
         {
             _device = device;
-
             Initialize();
         }
 
@@ -61,15 +60,17 @@ namespace tmp102
 
         protected void SendNibble(byte command)
         {
-            Span<byte> buffer = stackalloc byte[] { 0x00, (byte)(command & ~(byte)ControlByteFlags.Enabled) };
+            var data = (byte)(command | (byte)(BacklightOn ? ControlByteFlags.Backlight : 0x00));
+
+            Span<byte> buffer = stackalloc byte[] { 0x00, data };
             _device.Write(buffer);
             WaitForNotBusy(24);
 
-            buffer[1] = (byte)(command | (byte)ControlByteFlags.Enabled);
+            buffer[1] = (byte)(data | (byte)ControlByteFlags.Enabled);
             _device.Write(buffer);
             WaitForNotBusy(24);
 
-            buffer[1] = (byte)(command & ~(byte)ControlByteFlags.Enabled);
+            buffer[1] = data;
             _device.Write(buffer);
             WaitForNotBusy(24);
         }
@@ -77,10 +78,9 @@ namespace tmp102
         public override void SendCommand(byte command)
         {
             // Wait for busy flag
-            var flag = (byte)(BacklightOn ? ControlByteFlags.Backlight : 0x00);
 
-            SendNibble((byte)((command & 0xF0) | flag));
-            SendNibble((byte)((command << 4) | flag));
+            SendNibble((byte)(command & 0xF0));
+            SendNibble((byte)(command << 4));
         }
 
         public override void SendCommands(ReadOnlySpan<byte> commands)
@@ -99,10 +99,9 @@ namespace tmp102
         public override void SendData(byte value)
         {
             // Wait for busy flag
-            var flag = (byte)(ControlByteFlags.Data | (BacklightOn ? ControlByteFlags.Backlight : 0x00));
 
-            SendNibble((byte)((value & 0xF0) | flag));
-            SendNibble((byte)((value << 4) | flag));
+            SendNibble((byte)((value & 0xF0) | (byte)ControlByteFlags.Data));
+            SendNibble((byte)((value << 4) | (byte)ControlByteFlags.Data));
         }
 
         public override void SendData(ReadOnlySpan<byte> values)
